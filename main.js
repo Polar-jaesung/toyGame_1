08a02f39a46e877e694667a7ed7c96bc9cb8c007
JaeSung Byun(Mac) <bbbjason@naver.com>
2022-10-30 20:58:26 +0700

    fixed rspX as OBJECT

--- main.js
+++ main.js
@@ -4,6 +4,11 @@ const 가위 = document.querySelector('#가위');
 const 바위 = document.querySelector('#바위');
 const 보 = document.querySelector('#보');
 
+const rspX = {
+ 가위x :'-0', //가위
+ 바위x :'-220px', //바위
+ 보x :'-440px', //보
+};
 const IMG_URL = './imgs/rsp.png';
 computer.style.background = `url(${IMG_URL})0 0`;
-computer.style.backgroundSize ='auto 200px';
+computer.style.backgroundSize ='auto 200px'; //이렇게 하면 이미지 사이즈게 높이200px에 맞춰서 '자동으로' 맞춰짐
