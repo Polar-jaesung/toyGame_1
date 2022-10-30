fe028d7e8c39328012657a08e91d33d71e919a42
JaeSung Byun(Mac) <bbbjason@naver.com>
2022-10-30 19:22:53 +0700

    add rsp img on CSS

---
+++ main.js
@@ -0,0 +1,9 @@
+const computer = document.querySelector('#computer');
+const score = document.querySelector('#score');
+const 가위 = document.querySelector('#가위');
+const 바위 = document.querySelector('#바위');
+const 보 = document.querySelector('#보');
+
+const IMG_URL = './imgs/rsp.png';
+computer.style.background = `url(${IMG_URL})0 0`;
+computer.style.backgroundSize ='auto 200px';
