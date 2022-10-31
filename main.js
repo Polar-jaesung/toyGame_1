b5daa61ae77cfc3a16fbf98823a38d0dc9c3ccd0
Jaesung <bbbjason@naver.com>
2022-10-31 11:52:59 +0700

    Code refactoring

--- main.js
+++ main.js
@@ -13,25 +13,20 @@ const IMG_URL = './imgs/rsp.png';
 computer.style.background = `url(${IMG_URL})0 0`;
 computer.style.backgroundSize ='auto 200px'; //이렇게 하면 이미지 사이즈게 높이200px에 맞춰서 '자동으로' 맞춰짐
 
-let coord ='0';
+let computerChoice ='가위x'; //객체를 굳이 지정하지 않아도 읽기 쉽게 코딩가능
 setInterval(()=>{
-    if(coord === rspX.가위x){ //'가위' 일때
-        coord = rspX.바위x;
-        computer.style.background = `url(${IMG_URL}) ${rspX.바위x} 0`;
-        computer.style.backgroundSize ='auto 200px'; 
+    if(computerChoice === '가위x'){ //'가위' 일때
+        computerChoice = '바위x';        
     }
 
-    else if(coord === rspX.바위x){//'바위' 일때
-        coord = rspX.보x;
-        computer.style.background = `url(${IMG_URL}) ${rspX.보x} 0`;
-        computer.style.backgroundSize ='auto 200px'; 
+    else if(computerChoice === '바위x'){//'바위' 일때
+        computerChoice = '보x';       
     }
 
-    else if (coord === rspX.보x){//'보' 일때
-        coord = rspX.가위x;
-        computer.style.background = `url(${IMG_URL}) ${rspX.가위x} 0`;
-        computer.style.backgroundSize ='auto 200px'; 
-    }
-
+    else if (computerChoice === '보x'){//'보' 일때
+        computerChoice = '가위x'  ;        
+    }  
     
+        computer.style.background = `url(${IMG_URL}) ${rspX[computerChoice]} 0`;
+        computer.style.backgroundSize ='auto 200px'; 
 },50);
